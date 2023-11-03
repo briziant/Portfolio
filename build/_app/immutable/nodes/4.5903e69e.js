ac3436ad1be78c2befd515bedbd447b1b4f43949
Anton K. (ai Doge) <aidoge@aidoge.xyz>
2023-11-03 04:03:58 -0400

    build stuff

---
+++ build/_app/immutable/nodes/4.5903e69e.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.d329acd7.js";import{S as t,i as e}from"../chunks/index.403eca08.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
---
+++ build/_app/immutable/nodes/7.7c49ac47.js
@@ -0,0 +1,12 @@
+import{s as g,n as _}from"../chunks/scheduler.d329acd7.js";import{S as $,i as v,g as m,s as f,r as w,h,x as u,c as d,u as x,k as C,a as r,v as y,d as z,t as P,f as c,w as T}from"../chunks/index.403eca08.js";import{P as b}from"../chunks/Project.0c002e2d.js";function j(i){let e,l=`The concept for this project was to create a modern house using only a
+		limited amount of materials. The main two being Concrete and Glass. From
+		there I wanted to have a simplistic design that was modern but still
+		felt lived in.`,o,n,a=`The design of the house was modeled so that the house would sit in the
+		sun line. Using this I strategically placed the bedroom and the kitchen
+		in the east facing side of the structure to allow for the morning to
+		light those rooms as you would naturally occupy them. I placed the
+		office in the west side of the home for the same reason, giving the
+		office the mid-day light and into the evening. The project was an
+		exercise in modern design, feng shui, as well as real world
+		implementations to try to build a house that would be interesting to
+		conceptualize`;return{c(){e=m("p"),e.textContent=l,o=f(),n=m("p"),n.textContent=a},l(t){e=h(t,"P",{"data-svelte-h":!0}),u(e)!=="svelte-1n3gi6s"&&(e.textContent=l),o=d(t),n=h(t,"P",{"data-svelte-h":!0}),u(n)!=="svelte-lxq2k2"&&(n.textContent=a)},m(t,s){r(t,e,s),r(t,o,s),r(t,n,s)},p:_,d(t){t&&(c(e),c(o),c(n))}}}function k(i){let e,l="<b>Puzzle Temple</b>",o,n,a;return n=new b({props:{cover:i[0],$$slots:{default:[j]},$$scope:{ctx:i}}}),{c(){e=m("span"),e.innerHTML=l,o=f(),w(n.$$.fragment),this.h()},l(t){e=h(t,"SPAN",{class:!0,"data-svelte-h":!0}),u(e)!=="svelte-wx37zm"&&(e.innerHTML=l),o=d(t),x(n.$$.fragment,t),this.h()},h(){C(e,"class","uppercase text-5xl")},m(t,s){r(t,e,s),r(t,o,s),y(n,t,s),a=!0},p(t,[s]){const p={};s&2&&(p.$$scope={dirty:s,ctx:t}),n.$set(p)},i(t){a||(z(n.$$.fragment,t),a=!0)},o(t){P(n.$$.fragment,t),a=!1},d(t){t&&(c(e),c(o)),T(n,t)}}}function I(i){return[{src:"/puzzle_temp.png",title:"Puzzle Temple"}]}class L extends ${constructor(e){super(),v(this,e,I,k,g,{})}}export{L as component};
