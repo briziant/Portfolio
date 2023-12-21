65db4be952fd1f65370a0a2645ba833cbe9026b1
briziant <humphrybryant@gmail.com>
2023-12-20 23:09:35 -0600

    changes

--- src/lib/Back.svelte
+++ src/lib/Back.svelte
@@ -6,7 +6,7 @@
 <div
 	class="relative w-full h-12 text-2xl flex items-center justify-between align-middle">
 	<a
-		class="flex-1 inline-flex gap-2 items-center align-middle text-pallet-text link hover:text-pallet-accent"
+		class="flex-1 inline-flex gap-2 items-center align-middle text-gray-300 link hover:text-pallet-accent"
 		href="{base}/">
 		<IconArrowNarrowLeft class="w-8 h-auto" />
 		<span class="uppercase"><b>back</b></span>
